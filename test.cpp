7a28d0b21a32421cd879cf7222997cc83aacb3f0
FahadNiazi420 <fkk420302@gmail.com>
2024-10-21 10:03:43 +0500

    Pass File as Argument

---
+++ test.cpp
@@ -0,0 +1,40 @@
+int main() {
+    int a = 5;
+    float b = 10.5;
+    if (a < b) {
+        return a + b;
+    } else {
+        return a;
+    }
+}
+int a;
+a = 5;
+
+int b;
+b = a + 10;
+
+Agar (b > 10) {
+    return b;
+} else {
+    return 0;
+}
+
+float f;
+f = 3.14;
+
+char c;
+c = 'x';
+
+bool isTrue;
+isTrue = true;
+
+for (int i = 0; i < 10; i = i + 1) {
+    b = b + i;
+}
+
+while (a < 20) {
+    a = a + 1;
+}
+
+string name;
+name = "SmartSketch";
